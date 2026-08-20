8ef3ef1e3b838e3fa18829d54fffe8ee601562af
purpleshark74 <aaravjayapalan@gmail.com>
2026-08-20 11:10:38 +0800

    feat(chatbot): add echo

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Bobby {
     public static void main(String[] args) {
         String banner = "BBBB   OOO   BBBB  BBBB  Y   Y\n"
@@ -5,13 +7,27 @@ public class Bobby {
                 + "BBBB  O   O  BBBB  BBBB    Y\n"
                 + "B   B O   O  B   B B   B   Y\n"
                 + "BBBB   OOO   BBBB  BBBB    Y";
-        System.out.println("-------------------------------------");
+        String LINE = "------------------------------------------";
+
+        System.out.println(LINE);
         System.out.println(banner);
-        System.out.println("-------------------------------------");
-        System.out.println("Hello, I'm Bobby.");
-        System.out.println("What can I do for you?");
-        System.out.println("-------------------------------------");
-        System.out.println("Bye! Hope to see you again soon. ");
-        System.out.println("-------------------------------------");
+        System.out.println(LINE);
+        System.out.println("     Hello, I'm Bobby.");
+        System.out.println("     What can I do for you?");
+        System.out.println(LINE);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.trim().equalsIgnoreCase("bye")) {
+            System.out.println(LINE);
+            System.out.println("     " + input);
+            System.out.println(LINE);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(LINE);
+        System.out.println("     Bye! Hope to see you again soon. ");
+        System.out.println(LINE);
     }
 }
