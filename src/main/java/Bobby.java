ec3c365e03b563070e0ec7ae51e5ff789ac44157
purpleshark74 <aaravjayapalan@gmail.com>
2026-08-20 10:42:54 +0800

    feat(chatbot): added greet and exit

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -5,6 +5,13 @@ public class Bobby {
                 + "BBBB  O   O  BBBB  BBBB    Y\n"
                 + "B   B O   O  B   B B   B   Y\n"
                 + "BBBB   OOO   BBBB  BBBB    Y";
+        System.out.println("-------------------------------------");
         System.out.println(banner);
+        System.out.println("-------------------------------------");
+        System.out.println("Hello, I'm Bobby.");
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------------");
+        System.out.println("Bye! Hope to see you again soon. ");
+        System.out.println("-------------------------------------");
     }
 }
